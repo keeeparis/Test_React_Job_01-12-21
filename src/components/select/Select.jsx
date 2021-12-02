1e7e50752b1995f5d7c6dfb0a8cc2886c47aa340
Vova_Garvae <vgarvae@gmail.com>
2021-12-03 01:16:54 +0300

    code-review

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -5,7 +5,7 @@ export default function Select({options, defaultValue, data, setData}) {
     const [optionsVisible, setOptionsVisible] = useState(false)
     const [displayValue, setDisplayValue] = useState(defaultValue)
 
-    useEffect(() => {
+    useEffect(() => { // хорошим тоном будет объявление эффекта в конце кода перед "return"
         window.addEventListener('click', handleClickSelect)
         return () => {
             window.removeEventListener('click', handleClickSelect)
@@ -16,30 +16,31 @@ export default function Select({options, defaultValue, data, setData}) {
         return !e.target.classList.contains(styles.select) && !e.target.classList.contains('option') ? setOptionsVisible(false) : null
     }
 
-    const handleClickOption = (e) => {
+    const handleClickOption = (e) => { // Соблюдай порядок объявления и использования функций. ES6 такое прощает, но делать так не стоит.
         setDisplayValue(e.target.innerText)
-        setOptionsVisible(false) 
+        setOptionsVisible(false)
         setData({...data, lang: e.target.dataset.value})
     }
-    
+
 
     return (
         <div className={styles.wrapper}>
             <label htmlFor="lang" className={styles.label}>{defaultValue}</label>
             <div className={styles.selectwrap}>
-                <div 
-                    className={optionsVisible ? `${styles.select} ${styles.active}` : styles.select} 
+                <div
+                    className={optionsVisible ? `${styles.select} ${styles.active}` : styles.select}
                     onClick={() => setOptionsVisible(true)}
                     language={displayValue}
                 >
                     {displayValue}
                 </div>
             </div>
+            {/* для корректного написания классов используй библиотеку https://github.com/lukeed/clsx */}
             <ul className={optionsVisible ? `${styles.options} ${styles.visible}` : styles.options}>
-                {options.map(option => 
+                {options.map(option =>
                     <li data-value={option.value} key={option.value} className='option' onClick={handleClickOption}>{option.label}</li>
                 )}
             </ul>
-        </div>  
+        </div>
     )
 }
